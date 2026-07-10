b1d5b1f55649230ea9442e0a8f9588f9c56b007e
Sai <gementizasgg08@gmail.com>
2026-07-11 04:04:50 +0800

    chore: linking of new pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,10 +6,9 @@ export default function Footer() {
       <div className="font-bold text-white text-xl tracking-tighter  w-1/3">OTUS</div>
       <div className="tracking-widest font-mono w-1/3 justify-center flex">© 2026 OTUS CEBU</div>
       <div className="flex gap-6 tracking-widest font-mono w-1/3 justify-end">
-        <a href="#" className="hover:text-white transition-colors">PRIVACY</a>
+        <a href="/privacy" className="hover:text-white transition-colors">PRIVACY</a>
         <a href="/termsOfService" className="hover:text-white transition-colors">TERMS</a>
-        <a href="#" className="hover:text-white transition-colors">CONTACT</a>
-        <a href="#" className="hover:text-white transition-colors">INSTAGRAM</a>
+        <a href="/faq" className="hover:text-white transition-colors">FAQ</a>
       </div>
     </footer>
   )
